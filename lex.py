771b725091025e9ca412b4c9d975b5e70b0845f1
Luis Carlos Marval <luiscarm77@gmail.com>
2019-09-29 19:17:49 -0400

    Initial commit

---
+++ lex.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python
+
+###############################################################
+# === Archivo: lex
+# === Lenguaje: Python
+# === Version: 2.7
+# === Septiembre-Diciembre 2019
+# === CI-3725
+# === Funcion: Analizador lexicografico del lenguaje GuardedUSB
+# === Autores:
+#       -Luis  Marval  12-10620
+#       -Fabio Suarez  12-10578
+################################################################
+
+
+
+import ply.lex as lex
+import re
+import codecs
+import os
+import sys
+
+arrToks = []
+errToks = []
+row = 0
+col = 0
+
+tokens = [  
+            'TkNum','TkId','TkPlus','TkMinus',
+            'TkMult','TkDiv','TkMod', 'TkPunto','TkSemicolon','TkConjuncion',
+            'TkDisyuncion','TkMenor','TkMenorIgual','TkMayor',
+            'TkMayorIgual','TkIgual','TkDesigualdad','TkSiguienteCar',
+            'TkAnteriorCar','TkValorAscii','TkConcatenacion','TkDosPuntos',
+            'TkShift','TkCorcheteAbre','TkCorcheteCierra','TkParAbre',
+            'TkParCierra','TkLlaveAbre','TkLlaveCierra','TkHacer',     
+            'TkAsignacion','TkComa','TkSaltoDeLinea', 'TkTab',              
+            'TkComillaSimple','TkBarraInversa','TkComments'
+]
+
+reservadas = {
+            'atoi':'TkAtoi',
+            'size':'TkSize',
+            'max':'TkMax',
+            'min':'TkMin',
+            'declare': 'TkDeclare',
+            'read': 'TkRead',
+            'print': 'TkPrint',
+            'println': 'TkPrintln',
+            'if':'TkIf',        
+            'fi': 'TkFi',
+            'for': 'TkFor',
+            'rof': 'TkRof',
+            'in': 'TkIn',
+            'to': 'TkTo',
+            'do':'TkDo',
+            'od':'TkOd',
+            'int':'TkInt',
+            'array':'TkArray',
+            'true':'TkTrue', 
+            'false':'TkFalse',
+}
+
+tokens = list(reservadas.values())+ ['TkOBlock','TkCBlock','TkSoForth', 'TkComma',
+         'TkOpenPar', 'TkCloserPar', 'TkAsig', 'TkSemicolon','TkArrow', 'TkPlus',
+         'TkMinus', 'TkMult', 'TkDiv', 'TkMod', 'TkPunto', 'TkOr', 'TkAnd', 'TkNot',
+         'TkLess', 'TkLeq', 'TkGreater', 'TkGeq', 'TkEqual', 'TkNEqual', 'TkOBracket',
+         'TkCBracket', 'TkTwoPoints', 'TkTwoPoints' , 'TkConcat', 'TkNum','TkId', 'TkUminus', 'TkConditional','TkComments']
+
+# Regular expression rules for simple tokens
+#t_TkCaracter   = r'\'[a-zA-Z]\''
+
+t_TkConditional = r'\[\]'
+t_TkOBlock = r'\|\['
+t_TkCBlock = r'\]\|'
+t_TkSoForth = r'\.\.'
+t_TkComma = r','
+t_TkOpenPar = r'\('
+t_TkCloserPar = r'\)'
+t_TkAsig = r'\:\='
+t_TkSemicolon = r';'
+t_TkArrow = r'\-\-\>'
+t_TkPlus       = r'\+'
+t_TkMinus      = r'-'
+t_TkMult       = r'\*'
+t_TkDiv        = r'/'
+t_TkMod        = r'%'
+t_TkPunto      = r'\.'
+t_TkOr = r'\/\\'
+t_TkAnd = r'\/\\'
+t_TkNot = r'!'
+t_TkLeq = r'<='
+t_TkGeq = r'>='
+t_TkLess = r'<'
+t_TkGreater = r'>'
+t_TkEqual = r'=='
+t_TkNEqual = r'!='
+t_TkOBracket = r'\['
+t_TkCBracket = r'\]'
+t_TkTwoPoints = r'\:'
+t_TkConcat = r'\|\|'
+t_TkComments = r'\/\/'
+#t_TkLineFeed = r'\n'
+#t_TkWhiteSpace = r'\s'
+
+# A regular expression rule with some action code
+def t_TkNum(t):
+    r'0|\d+'
+    t.value = int(t.value)    
+    return t
+
+def t_TkId(t):
+    r'[a-zA-Z][a-zA-Z0-9_]*'
+    if t.value in reservadas:
+        t.type = reservadas.get(t.value, None)
+    return t
+
+def t_TkUminus(t):
+    r'-[a-zA-Z|1-9]*[a-zA-Z|0-9]*'
+    if t.value in reservadas:
+        t.type = reservadas.get(t.value, None)
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# Ignoramos espacios y tabs ( Faltan saltos de linea y comentarios)
+t_ignore  = ' \t'
+#t_ignore_jumpLine = r'\n'
+
+# Compute column.
+#     input is the input text string
+#     token is a token instance
+def find_column(input, token):
+    line_start = input.rfind('\n', 0, token.lexpos) + 1
+    return (token.lexpos - line_start) + 1
+
+
+# Funcion: t_error
+# Maneja el proceso de tokens no validos
+#almacenandolos en una lista.
+def t_error(t):
+    temp = "Error: Unexpected character "+str(t.value[0])+" in row "+str(row)+", column "+ str(t.lexpos+1)
+    errToks.append(temp)
+    t.lexer.skip(1)
+
+def imprimir():
+    if len(errToks) != 0:
+        for i in range(len(errToks)):
+            if str(errToks[i])!="":
+                print errToks[i]
+    else:    
+        for i in range(len(arrToks)):
+            if str(arrToks[i])!= "":
+                print arrToks[i]
+
+# Funcion: inputError
+#Return:
+#   -True si existe parametro de entrada desde terminal
+#   -False en caso contrario
+def inputError():
+    if (len(sys.argv)==2):
+        return True
+    else:
+        return False
+
+# Funcion: exist_File
+# Entrada:
+#   -String que representa el nombre de un archivo
+# Return:
+#   -True si existe un archivo con el nombre ingresado
+#   -False en caso contrario
+
+def exist_File(fname):
+    if (os.path.isfile(fname)):
+        return True
+    else:
+        return False
+
+#Funcion Principal main
+def main():
+
+    # Build the lexer
+    lex.lex()
+    if (inputError()):
+        first_arg = sys.argv[1]
+        if (exist_File(first_arg)):
+            archivo = open(first_arg)
+            linea= archivo.readline()
+            contador = 0
+            global row 
+            while linea != '':
+                # procesar linea
+                lex.input(linea)
+                row= row+1
+                arrToks.append("")
+
+                while True:
+                    tok = lex.token()
+                    if not tok or tok.type == "TkComments" : break
+                    else:
+                        col = tok.lexpos + 1
+
+                        if tok.type == "TkId":
+                            temp = str(tok.type)+"(\""+str(tok.value)+"\")"+" "+str(row)+" "+str(col)
+                            arrToks.append(temp)
+
+                        elif tok.type == "TkNum":
+                            temp = str(tok.type)+"(\""+str(tok.value)+"\")"+" "+str(row)+" "+str(col)
+                            arrToks.append(temp)
+                        
+                        else:
+                            temp = str(tok.type)+" "+str(row)+" "+str(col)
+                            arrToks.append(temp)
+
+                contador +=1
+                linea=archivo.readline()
+            
+            imprimir()
+        
+        else:
+            print("IOError: el archivo \""+str(first_arg)+"\" no se encuentra en el directorio actual") 
+    
+    else:
+        print("Error: Entrada vacia")
+
+
+#main()
+
+if __name__ == '__main__':
+      main()
